9e6f9b8c819b6c7954b1a67cffd4789e5542eb39
Jojaah12 <david.aljojah@gmail.com>
2024-03-08 10:03:51 +0100

    Add project files.

---
+++ Encapsulation-inheritance-polymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Encapsulation_inheritance_polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
