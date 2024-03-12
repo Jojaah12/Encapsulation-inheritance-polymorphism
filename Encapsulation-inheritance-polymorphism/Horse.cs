c04718858b200a0703b78f18c44e6eb4657339a0
Jojaah12 <david.aljojah@gmail.com>
2024-03-12 15:54:19 +0100

    Added Horse subclass with Color prop and MakeSound method

---
+++ Encapsulation-inheritance-polymorphism/Horse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_inheritance_polymorphism
+{
+    public class Horse : Animal
+    {
+        // unique prop for horse
+        public string Color { get; set; }
+
+        // Construtor
+        public Horse(string name, double weghit, int age, string color) : base(name, weghit, age)
+        {
+            Color = color;
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Clip clop");
+        }
+    }
+}
