5891b74c308e70b12be955c5306177c8948f8968
Jojaah12 <david.aljojah@gmail.com>
2024-03-12 17:57:49 +0100

    Implemented Wolfman class inheriting from the Wolf and implmented IPerson interface

---
+++ Encapsulation-inheritance-polymorphism/Wolfman.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_inheritance_polymorphism
+{
+    public class Wolfman : Wolf, IPerson
+    {
+        public Wolfman(string name, double weight, int age) : base(name, weight, age, 0)
+        {
+        }
+
+        public void Talk()
+        {
+            Console.WriteLine("I am a Wolfman, and I can talk!");
+        }
+    }
+}
+
