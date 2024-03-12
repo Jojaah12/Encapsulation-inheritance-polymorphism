ca6c9e47f678e1afc148d9ff8376d19e006edae9
Jojaah12 <david.aljojah@gmail.com>
2024-03-12 16:17:36 +0100

    implemented Dog subclass

--- Encapsulation-inheritance-polymorphism/Dog.cs
+++ Encapsulation-inheritance-polymorphism/Dog.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace Encapsulation_inheritance_polymorphism
 {
-    public class Horse : Animal
+    public class Dog : Animal
     {
         // unique prop for horse
         public string Color { get; set; }
 
         // Construtor
-        public Horse(string name, double weghit, int age, string color) : base(name, weghit, age)
+        public Dog(string name, double weghit, int age, string color) : base(name, weghit, age)
         {
         }
 
         public override void DoSound()
         {
-            Console.WriteLine("Clip clop");
+            Console.WriteLine("woff woff");
         }
     }
 }
