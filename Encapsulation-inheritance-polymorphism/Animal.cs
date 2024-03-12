95df015b6d003d3d40c271d9680749f6a29c1484
Jojaah12 <david.aljojah@gmail.com>
2024-03-12 15:45:55 +0100

    Added Animal class with common props and abstract MakeSound method

---
+++ Encapsulation-inheritance-polymorphism/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_inheritance_polymorphism
+{
+    public abstract class Animal
+    {
+        // Properties
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int Age { get; set; }
+
+        // Constructor
+        public Animal(string name, double weghit, int age)
+        {
+            Name = name;
+            Weight = weghit;
+            Age = age;
+        }
+
+        // Abstract method making sound
+        public abstract void MakeSound();
+
+    }
+}
