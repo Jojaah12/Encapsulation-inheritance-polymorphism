573dc5beaff4e53a1051013f66365161593e7e81
Jojaah12 <david.aljojah@gmail.com>
2024-03-12 11:10:43 +0100

    Added NumericInputError and TextInputError  classes with UEMessage override

---
+++ Encapsulation-inheritance-polymorphism/TextInputError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_inheritance_polymorphism
+{
+    public class TextInputError : UseError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a text inoput iun a numeric only field";
+        }
+    }
+}
