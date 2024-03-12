2d27f829705b3fed8e8be977ab28d03bad068e1d
Jojaah12 <david.aljojah@gmail.com>
2024-03-12 16:26:21 +0100

    Implemented subclasses for the rest of the animals

---
+++ Encapsulation-inheritance-polymorphism/Wolf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_inheritance_polymorphism
+{
+    public class Wolf : Animal
+    {
+        public Wolf(string name, double weight, int age) : base(name, weight, age)
+        {
+        }
+
+        public override void DoSound()
+        {
+            Console.WriteLine("Howl howl");
+        }
+    }
+}
---
+++ Encapsulation-inheritance-polymorphism/Worm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_inheritance_polymorphism
+{
+    public class Worm : Animal
+    {
+        public Worm(string name, double weight, int age) : base(name, weight, age)
+        {
+        }
+
+        public override void DoSound()
+        {
+            Console.WriteLine("Squirm squirm");
+        }
+    }
+}
